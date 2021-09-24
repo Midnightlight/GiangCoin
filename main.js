d53024da6ef028e21d3d669fe48983c840b24fc6
Midnightlight <vegiangle2602@gmail.com>
2021-09-24 21:56:57 +0300

    update

--- main.js
+++ main.js
@@ -7,16 +7,28 @@ class Block {
         this.data = data;
         this.previousHash = previousHash;
         this.hash = this.calculateHash;
+        this.nonce = 0;
     }
 
     calculateHash() {
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+    }
+
+    mineBlock(difficulty) {
+        while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+            this.nonce++;
+            this.hash = this.calculateHash;
+        }
+
+        console.log("Block mined: " + this.hash);
     }
 }
+
 
 class Blockchain { // create a blockchain
     constructor() {
         this.chain = [this.createGenesisBlock()];
+        this.difficulty = 2;
     }
 
     createGenesisBlock() {
@@ -29,7 +41,7 @@ class Blockchain { // create a blockchain
 
     addBlock(newBlock) {
         newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
+        newBlock.mineBlock(this.difficulty);
         this.chain.push(newBlock);
     }
 
@@ -55,10 +67,12 @@ let giangCoin = new Blockchain();
 giangCoin.addBlock(new Block(1, "10/07/2017", { amount: 4 }));
 giangCoin.addBlock(new Block(2, "12/07/2017", { amount: 10 }));
 
+
 console.log('Is blockchain valid? ' + giangCoin.isChainValid());
 
+console.log('Changing a block... ')
 giangCoin.chain[1].data = { amount: 100 };
-giangCoin.chain[1].hash = giangCoin.chain[1].calculateHash();
+// giangCoin.chain[1].hash = giangCoin.chain[1].calculateHash();
 
 console.log('Is blockchain valid? ' + giangCoin.isChainValid());
 
